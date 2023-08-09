77fd385a1183749822d6107faaf9fc159474a547
Alex Semin <alllexsm@gmail.com>
2023-08-09 23:56:57 +0200

    Next snapshot

--- tbot-api/build.gradle.kts
+++ tbot-api/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 group = "me.alllex.telegram.botkit"
-version = "0.1.0"
+version = "0.1.1-SNAPSHOT"
 
 val javaVersion: String by project
 java.toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)

11c3b8181369d56c9c7fcb1566de248b9cb99af5
Alex Semin <alllexsm@gmail.com>
2023-09-22 23:06:19 +0200

    Liberal tooling, conservative library

--- tbot-api/build.gradle.kts
+++ tbot-api/build.gradle.kts
@@ -1,6 +1,11 @@
 @file:Suppress("UnstableApiUsage")
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+
 plugins {
+    `java-library`
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlinx.binaryCompatibilityValidator)
@@ -14,8 +19,33 @@ repositories {
 group = "me.alllex.telegram.botkit"
 version = "0.3.0-SNAPSHOT"
 
-val javaVersion: String by project
-java.toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
+java.toolchain.languageVersion = libs.versions.jvmToolchain.map { JavaLanguageVersion.of(it) }
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release = libs.versions.jdkTarget.map { it.toInt() }
+}
+
+kotlin {
+    val kotlinTarget = libs.versions.kotlinTarget
+    val kotlinVersion = kotlinTarget.map { KotlinVersion.fromVersion(it.toKotlinMinor()) }
+    compilerOptions {
+        languageVersion = kotlinVersion
+        apiVersion = kotlinVersion
+    }
+    coreLibrariesVersion = kotlinTarget.get()
+
+    compilerOptions {
+        jvmTarget = libs.versions.jdkTarget.map { JvmTarget.fromTarget(it.toJdkTarget()) }
+        // https://kotlinlang.org/docs/compiler-reference.html#xjdk-release-version
+        freeCompilerArgs.add(libs.versions.jdkTarget.map { "-Xjdk-release=${it.toJdkTarget()}" })
+    }
+}
+
+// 8 => 1.8, 11 => 11
+fun String.toJdkTarget() = if (toInt() <= 8) "1.$this" else this
+
+// 1.7.21 => 1.7, 1.9 => 1.9
+fun String.toKotlinMinor() = split(".").take(2).joinToString(".")
 
 dependencies {
     implementation(libs.kotlinx.serialization.json)

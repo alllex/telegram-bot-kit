2f7a6c5c0b3313894b028680d16302b4f30af4be
Alex Semin <alllexsm@gmail.com>
2024-05-09 22:36:14 +0200

    Upgrade foojay resolver plugin to 0.8.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradle.develocity") version "3.17.2"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "tbot-api"

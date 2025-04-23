6df021765658226c02eff8f9b25b442ea1a94ebd
Alex by Software <software@alllex.me>
2025-04-23 11:57:02 +0200

    Update Foojay resolver plugin to 0.10.0
    Incremented the Foojay resolver plugin version from 0.8.0 to 0.10.0 in `settings.gradle.kts`. This ensures compatibility with recent updates and leverages the latest features and fixes.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradle.develocity") version "4.0"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
 
 rootProject.name = "tbot-api"
